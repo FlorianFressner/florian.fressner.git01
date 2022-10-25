9e021a4515765834e097c3a7307203060adada09
Flopinator <flo.fressner@gmail.com>
2022-10-25 20:19:59 +0200

    Erweiterung der Ausgabe mit Matrikelnummer

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,5 +2,8 @@ public class HelloGit {
 
     public static void main(String[] args) {
         System.out.println("Hello Git");
+
+        System.out.println("Matrikelnummer: 01605148");
+
     }
 }
